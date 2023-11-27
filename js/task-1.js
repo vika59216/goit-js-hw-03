9fdfed0f24d282dbf0ddab9bc383f26ea4fd0673
vika59216 <viktoriiadupreez@gmail.com>
2023-11-27 16:00:39 +0100

    hw3

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,13 @@
-
-
-function makeTransaction(quantity, pricePerDroid) {
-     const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function slugify(title) {
+    return title.toLowerCase().split(" ").join("-") ;
+};
     
-    }
-   
+    
+     
 
 
-    makeTransaction(5, 3000);
-    makeTransaction(3, 1000);
-    makeTransaction(10, 500);
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,24 @@
+function filterArray(numbers, value) {
+    let array = [];
+    for (let num of numbers) {
+        if (num > value) {
+            array.push(num);
+          
 
 
-function getElementWidth(content, padding, border) {
-    return parseFloat(content) + 2 * (parseFloat(padding) + parseFloat(border));
-     }
+              
+        }
+        
+    }
+      return array;
+}
 
 
 
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-    console.log(getElementWidth("200px", "0px", "0px")); 
 
-   
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
